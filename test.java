96719d9be5fa9765f7f7c4c9fc07fa007d59f182
Your Name <mshivendra462@gmail.com>
2023-03-29 17:27:07 +0530

    for test

---
+++ test.java
@@ -0,0 +1,23 @@
+
+
+ class Test {
+   public static void main(String[] args) {
+    String s="naman";
+    String s2="manan";
+    String s3="sanan";
+    try{
+
+    for(int i=0;i<s.length();i++)
+    {
+        Thread.sleep(1000);
+      System.out.print(s.charAt(i));
+    }
+
+   } 
+
+catch(Exception e)
+{
+  e.printStackTrace();
+}
+}
+}
