7ddc47ebb5bde39c5942e6659c5fb1df41f854c0
Your Name <mshivendra462@gmail.com>
2023-03-29 17:51:04 +0530

    vikram

---
+++ test1.java
@@ -0,0 +1,23 @@
+class Main {
+    public static void main(String[] args) {
+     String s="naman";
+     String s2="manan";
+     String s3="sanan";
+    
+ 
+     for(int i=0;i<s.length();i++)
+     {
+       for(int j=0;j<s2.length();j++)
+       {
+        for(int k=0;k<s3.length();k++)
+        {
+         
+        } 
+       } 
+       
+     }
+ 
+
+ }
+ }
+ 
